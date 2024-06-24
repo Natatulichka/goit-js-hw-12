43c265aca3ae76680e6d7a52bac650535d3897d5
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-06-24 13:22:28 +0200

    hw-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 export async function getQuery(value, page, perPage) {
   const params = new URLSearchParams({
     key: '44351431-da99bbc5aa576d6c36cc46a59',
@@ -11,6 +11,6 @@ export async function getQuery(value, page, perPage) {
     page: page,
   });
 
-  const res = await axios.get(`https://pixabay.com/api/?${params}`);
+  const res = await axios(`/?${params}`);
   return res.data;
 }
